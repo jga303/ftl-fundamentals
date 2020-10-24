458ddc6902be2bc00b9d4e343fc1af2ee8b8447b
Juan GARRIDO <jgarrido@pictet.com>
2020-10-24 14:48:57 +0200

    jga exerc -> chap.6

--- calculator.go
+++ calculator.go
@@ -1,13 +1,28 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
+import "fmt"
+
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
-return a + b
+	return a + b
 }
 
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
 func Subtract(a, b float64) float64 {
-	return b - a
+	return a - b
+}
+
+func Multiply(a, b float64) float64 {
+	return a * b
+}
+
+func Divide(a, b float64) (float64, error) {
+	if b != 0 {
+		return a / b, nil
+	} else {
+		return 0, fmt.Errorf("bad input: %f, %f (division by zero is undefined", a, b)
+	}
+
 }
--- calculator_test.go
+++ calculator_test.go
@@ -5,20 +5,92 @@ import (
 	"testing"
 )
 
+type testCase struct {
+	a, b float64
+	want float64
+	desc string
+}
+
+type testCaseWithErr struct {
+	a, b        float64
+	want        float64
+	desc        string
+	errExpected bool
+}
+
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+
+	testCases := []testCase{
+		{a: 2, b: 5, want: 7, desc: "Sum two positives"},
+		{a: -6, b: 2, want: -4, desc: "Sum a negative and a positive"},
+		{a: -5, b: -20, want: -25, desc: "Sum two negatives"},
+		{a: 2.5, b: -1.5, want: 1, desc: "Sum fractions"},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Add(%.2f, %.2f): want %.2f, got %.2f (%s)", tc.a, tc.b, tc.want, got, tc.desc)
+		}
 	}
 }
 
-// func TestSubtract(t *testing.T) {
-// 	t.Parallel()
-// 	var want float64 = 2
-// 	got := calculator.Subtract(4, 2)
-// 	if want != got {
-// 		t.Errorf("want %f, got %f", want, got)
-// 	}
-// }
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+
+	testCases := []testCase{
+		{a: 2, b: 5, want: -3, desc: "Subtract two positives"},
+		{a: -6, b: 8, want: -14, desc: "Subtract a negative and a positive"},
+		{a: -5, b: -20, want: 15, desc: "Subtract two negatives"},
+		{a: 2.3, b: -1.5, want: 3.8, desc: "Subtract fractions"},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Subtract(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Subtract(%.2f, %.2f): want %.2f, got %.2f (%s)", tc.a, tc.b, tc.want, got, tc.desc)
+		}
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+
+	testCases := []testCase{
+		{a: 3, b: 0, want: 0, desc: "Multiply two positives"},
+		{a: -3, b: +2, want: -6, desc: "Multiply a negative and a positive"},
+		{a: -5, b: -2, want: 10, desc: "Multiply two negatives"},
+		{a: 2.5, b: -1.5, want: -3.75, desc: "Multiply fractions"},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Multiply(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Multiply(%.2f, %.2f): want %.2f, got %.2f (%s)", tc.a, tc.b, tc.want, got, tc.desc)
+		}
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+
+	testCases := []testCase{
+		{a: 3, b: 2, want: 1.5, desc: "Divide two positives"},
+		{a: -7, b: +2, want: -3.5, desc: "Divide a negative and a positive"},
+		{a: -5, b: -2, want: 2.5, desc: "Divide two negatives"},
+		{a: 4.5, b: -1.5, want: -3, desc: "Divide fractions"},
+	}
+
+	for _, tc := range testCases {
+		got, err := calculator.Divide(tc.a, tc.b)
+		if err != nil {
+			if tc.want != got {
+				t.Errorf("Divide(%.2f, %.2f): want %.2f, got %.2f (%s)", tc.a, tc.b, tc.want, got, tc.desc)
+			}
+		} else {
+			t.Errorf("error: %v", err)
+		}
+	}
+
+}
